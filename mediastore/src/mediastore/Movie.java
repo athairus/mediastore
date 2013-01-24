003e3c7f3c817cad575070918f5705e143f63d76
Ryan M. Smith <rsmith22022@yahoo.com>
2013-01-24 15:47:45 -0500

    Added Movie subclass

---
+++ mediastore/src/mediastore/Movie.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mediastore;
+
+/**
+ *
+ * @author Ryan Smith
+ */
+public class Movie extends Media{
+    
+}
