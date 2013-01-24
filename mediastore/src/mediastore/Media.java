9732f40bf3fac8463ca7589ead394784c18ebf70
whiplash <whiplash000@gmail.com>
2013-01-24 15:35:49 -0500

    Added Media superclass, added Album class

---
+++ mediastore/src/mediastore/Media.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mediastore;
+
+/**
+ *
+ * @author Milton John
+ */
+public abstract class Media {
+
+    String author;
+    String title;
+    int duration; // in seconds
+    String genre;
+    int ranking; // 0 means not ranked
+    double price;
+}
