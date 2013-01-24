349863d2207f5f6e7dd62100ef1bc43e0460333c
whiplash <whiplash000@gmail.com>
2013-01-24 15:49:42 -0500

    Cleared out main

--- mediastore/src/mediastore/TestDriver.java
+++ mediastore/src/mediastore/TestDriver.java
@@ -14,6 +14,6 @@ public class TestDriver {
      * @param args the command line arguments
      */
     public static void main( String[] args ) {
-        System.out.println( false );
+        
     }
 }
