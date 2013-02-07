cb7012d247cca235401a242ec183289019fc3548
Cole Arnold <cma5357@psu.edu>
2013-02-07 10:03:57 -0500

    Added  empty Customer and Manager class

---
+++ mediastore/src/mediastore/Customer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mediastore;
+
+/**
+ *
+ * @author max7b_000
+ */
+public class Customer {
+    
+}
