7518b1f11b989e9c3905ddec8d25db0f85750b47
Ryan M. Smith <rsmith22022@yahoo.com>
2013-02-07 11:54:00 -0500

    Added data members and methods to Movie class

--- mediastore/src/mediastore/Movie.java
+++ mediastore/src/mediastore/Movie.java
@@ -4,5 +4,35 @@ package mediastore;
  * A class that encapsulates a movie.
  * @author Ryan Smith
  */
+
 public class Movie extends Media {
+
+    private String director; //director of the movie
+    private int releaseYear; //year of movie's release
+    
+    public Movie()
+    {   //POST: Initializes data members to "" or 0
+        director = "";
+        releaseYear = 0;
+    }
+
+ //****************************************************************************
+ //*                            Set and Get Methods                           *
+ //****************************************************************************
+    
+    public void setDirector(String director)
+    { //POST: Takes string and sets director to indicated string
+        this.director = director;
+    }
+    
+    public void setReleaseYear(int releaseYear)
+    {
+        this.releaseYear = releaseYear;
+    }
+    
+    
+    
+    
+    
+    
 }
