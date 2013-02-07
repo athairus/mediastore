dd27bb70c4a34c05e15d970217cfc30a501dd01f
Cole Arnold <cma5357@psu.edu>
2013-02-07 11:27:02 -0500

    added more methods to Customer.java (no changes to Manager.java)
    Added methods for customer to buy audiobooks, albumns  and movies.
    added methods to realloacate array size if too many audiobooks albumns
    and movies are added

--- mediastore/src/mediastore/Customer.java
+++ mediastore/src/mediastore/Customer.java
@@ -1,12 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mediastore;
 
 /**
  *
- * @author max7b_000
+ * @author Cole Arnold
  */
 public class Customer {
     String name;
@@ -18,19 +14,25 @@ public class Customer {
     Album[] albumsOwned;
     AudioBooks[] audioBooksOwned;
     Movie[] moviesOwned;
+    int albumIndex; // keeps track of current index in album array
+    int audioIndex;
+    int movieIndex;
     
-    public Customer(String name, String emailAddress, int age, int birthdate, int moneySpent){
+    public Customer(String name, String emailAddress, int age, int birthdate){
         this.name = name;
         this.emailAddress = emailAddress;
         this.age = age;
         this.birthdate = birthdate;
-        this.moneySpent = moneySpent;
+        this.moneySpent = 0;
         albumsOwned = new Album[startingSize];
         audioBooksOwned = new AudioBooks[startingSize];
         moviesOwned = new Movie[startingSize];
+        albumIndex = 0;
+        audioIndex = 0;
+        movieIndex = 0;
     }
     
-    public Album[] reallocateAlbumnSize(Album[] albumsOwned){
+    public Album[] reallocateAudioBooksSize(AudioBooks[] AubioBooksOwned){
         int oldSize = startingSize;
         this.startingSize = startingSize*2;
         albumsOwned = new Album[startingSize];
@@ -38,5 +40,38 @@ public class Customer {
         
         return albumsOwned;
     }
+    
+    public Movie[] reallocateMovieSize(Movie[] moviesOwned){
+        int oldSize = startingSize;
+        this.startingSize = startingSize*2;
+        moviesOwned = new Movie[startingSize];
+        System.arraycopy(this.moviesOwned,0,moviesOwned,0,oldSize);
+        
+        return moviesOwned;
+    }
+    public AudioBooks[] reallocateAlbumSize(AudioBooks[] audioBooksOwned){
+        int oldSize = startingSize;
+        this.startingSize = startingSize*2;
+        audioBooksOwned = new AudioBooks[startingSize];
+        System.arraycopy(this.audioBooksOwned,0,audioBooksOwned,0,oldSize);
+        
+        return audioBooksOwned;
+    }
+    
+    public void buyAlbum(Album newAlbum){
+        albumsOwned[albumIndex] = newAlbum;
+        albumIndex++;
+    }
+    
+    public void buyMovie(Movie newMovie){
+        moviesOwned[movieIndex] = newMovie;
+        movieIndex++;
+    }
+    
+    public void buyAudioBook(AudioBooks newAudioBooks){
+        audioBooksOwned[audioIndex] = newAudioBooks;
+        audioIndex++;
+    }
+    
     
 }
