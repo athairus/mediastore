74f411da9e4d707b35e957d92f786d33cee6bcfe
whiplash <whiplash000@gmail.com>
2013-02-11 09:04:02 -0500

    Fixed movie class
    And now it will compile...

--- mediastore/src/mediastore/Movie.java
+++ mediastore/src/mediastore/Movie.java
@@ -8,13 +8,13 @@ package mediastore;
 public class Movie extends Media {
 
     private int releaseYear; //year of movie's release
+    private String director;
 
     public Movie() {
         //POST: Initializes data members to "" or 0
         super();
         releaseYear = 0;
     }
-<<<<<<< HEAD
 
     public Movie( String author, String title, int duration, String genre, int ranking, double price, String director, int releaseYear ) {
         //PRE: Takes an author, title, duration, genre, ranking, price ( > 0 ), director of movie, and release year ( > 0 )
@@ -33,20 +33,10 @@ public class Movie extends Media {
         this.director = director;
     }
 
-    public void setReleaseYear( int releaseYear ) {
-        //PRE: Parameter takes in year of movie release
-        //POST: Release year is set to given value
-        this.releaseYear = releaseYear;
-    }
-
     public String getDirector() {
         //POST: returns movie's director
         return director;
     }
-
-    public int getReleaseYear() {
-        //POST: returns movie's release year
-=======
     
     public Movie(String author, String title, int duration, String genre, double price, int releaseYear){
       //PRE: Takes an author (director), title, duration, genre, ranking, price ( > 0 ), and release year ( > 0 )
@@ -69,7 +59,6 @@ public class Movie extends Media {
     
     public int getReleaseYear(){
       //POST: returns movie's release year
->>>>>>> Added default constructor and initializer constructor in Album and AudioBooks class, began add method in manager, edited movie class
         return releaseYear;
     }
 
