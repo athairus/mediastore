100bfc9afb05f9944dc524c2821ed3d39b35d943
Ryan M. Smith <rsmith22022@yahoo.com>
2013-02-14 17:27:41 -0500

    Fixed Add method in Manager class

--- mediastore/src/mediastore/Manager.java
+++ mediastore/src/mediastore/Manager.java
@@ -38,9 +38,13 @@ public class Manager {
                 int albumDuration = Integer.parseInt( input.nextLine() );
                 System.out.print( "Enter the album's genre: " );
                 String albumGenre = input.nextLine();
+                System.out.print(  "Enter the album's rating (out of 5 stars): ");
+                int albumRating = Integer.parseInt( input.nextLine() );
+                System.out.print( "Enter the album's total number of reviews: ");
+                int totalAlbumReviews = Integer.parseInt( input.nextLine() );
                 System.out.print( "Enter the album's price: " );
                 double albumPrice = Double.parseDouble( input.nextLine() );
-                Media newAlbum = new Album( artist, albumTitle, albumDuration, albumGenre, albumPrice, 0 );
+                Media newAlbum = new Album( artist, albumTitle, albumDuration, albumGenre, albumRating, totalAlbumReviews, albumPrice, 0 );
                 db.writeNewMediaItem( newAlbum );
 
                 break;
@@ -53,26 +57,34 @@ public class Manager {
                 int movieDuration = Integer.parseInt( input.nextLine() );
                 System.out.print( "Enter the movie genre: " );
                 String movieGenre = input.nextLine();
+                System.out.print(  "Enter the movie's rating (out of 5 stars): ");
+                int movieRating = Integer.parseInt( input.nextLine() );
+                System.out.print( "Enter the movie's total number of reviews: ");
+                int totalMovieReviews = Integer.parseInt( input.nextLine() );
                 System.out.print( "Enter the movie price: " );
                 double moviePrice = Double.parseDouble( input.nextLine() );
                 System.out.print( "Enter the movie release year: " );
                 int movieYear = Integer.parseInt( input.nextLine() );
-                Media newMovie = new Movie( director, movieTitle, movieDuration, movieGenre, moviePrice, movieYear, 0 );
+                Media newMovie = new Movie( director, movieTitle, movieDuration, movieGenre, movieRating, totalMovieReviews, moviePrice, movieYear, 0 );
                 db.writeNewMediaItem( newMovie );
 
                 break;
             case 3:
-                System.out.print( "Enter the audiobook author: " );
+                System.out.print( "Enter the book's author: " );
                 String author = input.nextLine();
-                System.out.print( "Enter the audiobook title: " );
+                System.out.print( "Enter the book's title: " );
                 String bookTitle = input.nextLine();
-                System.out.print( "Enter the audiobook duration: " );
+                System.out.print( "Enter the book's duration: " );
                 int bookDuration = Integer.parseInt( input.nextLine() );
-                System.out.print( "Enter the audiobook genre: " );
+                System.out.print( "Enter the book's genre: " );
                 String bookGenre = input.nextLine();
-                System.out.print( "Enter the audiobook price: " );
+                System.out.print(  "Enter the book's rating (out of 5 stars): ");
+                int bookRating = Integer.parseInt( input.nextLine() );
+                System.out.print( "Enter the book's total number of reviews: ");
+                int totalBookReviews = Integer.parseInt( input.nextLine() );
+                System.out.print( "Enter the book's price: " );
                 double bookPrice = Double.parseDouble( input.nextLine() );
-                Media newAudiobook = new Audiobook( author, bookTitle, bookDuration, bookGenre, bookPrice, 0 );
+                Media newAudiobook = new Audiobook( author, bookTitle, bookDuration, bookGenre, bookRating, totalBookReviews, bookPrice, 0 );
                 db.writeNewMediaItem( newAudiobook );
 
                 break;
