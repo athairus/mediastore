134b7e99275a9f7e4baeefab7d87e33debb10560
Ryan M. Smith <rsmith22022@yahoo.com>
2013-02-15 22:00:04 -0500

    CustomerCLITestDriver is not reading Database in folder 'db'

--- mediastore/src/mediastore/CustomerCLITestDriver.java
+++ mediastore/src/mediastore/CustomerCLITestDriver.java
@@ -1,6 +1,7 @@
 package mediastore;
 
 import java.io.File;
+import java.util.LinkedList;
 
 /**
  * Name: Milton John
@@ -26,7 +27,10 @@ public class CustomerCLITestDriver {
             e.printStackTrace(); // this is what the @SupressWarnings is for
         }
         try {
-            
+             if ( db.customers.isEmpty() ) {
+                db.writeNewCustomer( new Customer( 1, "test", "123 Fake St.", 200, new LinkedList(), db ) );
+            }
+            db.getCustomerFromID( 1 ).listCLI(); //doesnt recognize existing DB 
             
         } catch ( Exception e ) {
             System.out.println( "An exception occured testing customer functionality. (" + e.toString() + ")" );
