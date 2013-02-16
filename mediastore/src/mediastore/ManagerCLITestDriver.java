e215af83e9c99d57dae7fc4e803473f10001ebbd
whiplash <whiplash000@gmail.com>
2013-02-15 22:49:46 -0500

    Updated manager test driver

--- mediastore/src/mediastore/ManagerCLITestDriver.java
+++ mediastore/src/mediastore/ManagerCLITestDriver.java
@@ -33,11 +33,13 @@ public class ManagerCLITestDriver {
                 db.writeNewCustomer( new Customer( 1, "test", "123 Fake St.", 200, new LinkedList(), db ) );
             }
             db.manager.add( new Movie( 10, "Jack Black", "Tenacious D isn't dead I swear", 104, "Documentary", 5, 300, 49.99, 200000, 2013 ), 10 );
+            db.manager.add( new Movie( 11, "Jack Black", "Tenacious D isn't dead I swear", 104, "Documentary", 5, 300, 49.99, 200000, 2013 ), 11 );
             System.out.println( "Movie id #10 has sold " + db.manager.getNumSales( 10 ) + " copies" );
             System.out.println( "Total sales in entire store: " + db.manager.getTotalNumSales() );
-            System.out.println( "Customer #1: " + db.manager.getCustomerInfo( 1 ) );
+            System.out.println( db.manager.getCustomerInfo( 1 ) );
             Thread.sleep( 1000L ); // some time so you can see that the item is actually present in the db
             db.manager.remove( 10 );
+            db.manager.remove( 11 );
 
 
         } catch ( Exception e ) {
