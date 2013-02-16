c61f2598296013395a8750c58ac95c30757610a7
whiplash <whiplash000@gmail.com>
2013-02-15 22:33:31 -0500

    Minor change to Manager test driver

--- mediastore/src/mediastore/ManagerCLITestDriver.java
+++ mediastore/src/mediastore/ManagerCLITestDriver.java
@@ -33,7 +33,6 @@ public class ManagerCLITestDriver {
                 db.writeNewCustomer( new Customer( 1, "test", "123 Fake St.", 200, new LinkedList(), db ) );
             }
             db.manager.add( new Movie( 10, "Jack Black", "Tenacious D isn't dead I swear", 104, "Documentary", 5, 300, 49.99, 200000, 2013 ), 10 );
-            db.getCustomerFromID( 1 ).listCLI();
             System.out.println( "Movie id #10 has sold " + db.manager.getNumSales( 10 ) + " copies" );
             System.out.println( "Total sales in entire store: " + db.manager.getTotalNumSales() );
             System.out.println( "Customer #1: " + db.manager.getCustomerInfo( 1 ) );
