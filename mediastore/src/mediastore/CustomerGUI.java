68f8523c91aae520e44cffb25b6063d5bed579e1
Cole Arnold <cma5357@psu.edu>
2013-03-16 13:10:17 -0400

    Added a CustomerGUI and ManagerGUI to the media store
    Created a CutomerGUI and ManagerGUI to create the indiviual user
    interfaces for customers and the manager of the media store. No real
    code has been written for them yet.

---
+++ mediastore/src/mediastore/CustomerGUI.java
@@ -0,0 +1,12 @@
+package mediastore;
+
+
+/**
+ * @author Milton John
+ * @version 1.0 Mar 16, 2013
+ */
+public class CustomerGUI {
+
+
+
+}
---
+++ mediastore/src/mediastore/ManagerGUI.java
@@ -0,0 +1,12 @@
+package mediastore;
+
+/*
+ * @author Milton John, Cole Arnold, Ryan Smith
+ * @version 1.0 Mar 16, 2013
+ *
+ */
+public class ManagerGUI {
+
+    
+
+}
