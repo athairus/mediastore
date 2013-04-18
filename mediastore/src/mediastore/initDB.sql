cf226d7529ffaad6a26d59bc51ce8ac0d7f17c54
whiplash <whiplash000@gmail.com>
2013-04-18 15:08:04 -0400

    Finalized empty database initialization again, updated gitignore

--- mediastore/src/mediastore/initDB.sql
+++ mediastore/src/mediastore/initDB.sql
@@ -3,44 +3,44 @@ create table manager (
 );
 
 create table media (
-	media_id integer,
+	media_id int not null primary key generated always as identity ( start with 1, increment by 1 ),
 	type char( 2 ),
 	author varchar( 256 ),
 	title varchar( 256 ),
-	duration integer,
+	duration int,
 	genre varchar( 256 ),
 	rating double,
-	total_reviews integer,
+	total_reviews int,
 	price double,
-	numsold integer,
-	releaseyear integer,
-	constraint pk_media_id primary key ( media_id )
+	numsold int,
+	releaseyear int/*,
+	constraint pk_media_id primary key ( media_id )*/
 );
 
 create table customer (
-	customer_id integer not null,
+	customer_id int not null primary key generated always as identity ( start with 1, increment by 1 ),
 	name varchar( 256 ),
 	address varchar( 256 ),
-	balance double,
-	constraint pk_customer_id primary key ( customer_id )
+	balance double/*,
+	constraint pk_customer_id primary key ( customer_id )*/
 );
 
 create table purchase (
-	purchase_id integer not null,
-	media_id integer,
+	purchase_id int not null primary key generated always as identity ( start with 1, increment by 1 ),
+	media_id int,
 	price double,
 	p_date date,
 	p_time time,
-	constraint pk_purchase_id primary key ( purchase_id ),
+	/*constraint pk_purchase_id primary key ( purchase_id ),*/
 	constraint fk_media_id foreign key ( media_id ) references media ( media_id )
 );
 
 create table customer_purchases (
-	customer_id integer,
-	purchase_id integer,
+	customer_id int,
+	purchase_id int,
 	constraint fk_customer_id foreign key ( customer_id ) references customer ( customer_id ),
 	constraint fk_purchase_id foreign key ( purchase_id ) references purchase ( purchase_id )
 );
 
 
-insert into customer( customer_id, name, address, balance ) values ( 1, 'George W. Bush', '123 Texas St.', 350.0 );
+insert into customer( name, address, balance ) values ( 'George W. Bush', '123 Texas St.', 350.0 );
